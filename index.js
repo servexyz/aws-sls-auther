7789c1a6211fcc55e1d457bae31c9e056ad16eac
alechp <me@alechp.com>
2018-05-31 11:12:47 -0700

    Changed to common export.. ES6 export throwing err

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 require("dotenv").config();
+
+const { isAuthorized } = require('./auth/orize.js'
+const { authenticate } = require('./auth/enticate.js'
+
+module.exports = { isAuthorized, authenticate } 
+
