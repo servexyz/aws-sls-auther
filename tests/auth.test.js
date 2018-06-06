341e495ca599bee8930e4d4be1c705e54a5c91a1
alechp <me@alechp.com>
2018-06-06 15:54:56 -0700

    Changed file path in auth.test.js to ensure export is working as expected

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,7 +1,9 @@
 import { serial as test } from "ava";
 import { AUTHER_MOCK_USERNAME, AUTHER_MOCK_PASSWORD } from "babel-dotenv";
-import { authenticate } from "../src/auth/enticate.js";
-import { isAuthorized } from "../src/auth/orize.js";
+// import { authenticate } from "../src/auth/enticate.js";
+// import { isAuthorized } from "../src/auth/orize.js";
+import { authenticate } from "../src/index.js";
+import { isAuthorized } from "../src/index.js";
 import chalk from "chalk";
 
 var token;
