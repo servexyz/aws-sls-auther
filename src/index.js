764207da5f602ae34d56c69413bc8aec91440bbc
alechp <me@alechp.com>
2018-06-10 10:39:57 -0700

    Works !
    Confirmed by testing/importing with npm-starter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log(`Local AWS-SLS-Auther 10`);
+console.log(`Local AWS-SLS-Auther 12`);
 
 export { authenticate } from "./auth/enticate.js";
 export { isAuthorized } from "./auth/orize.js";
